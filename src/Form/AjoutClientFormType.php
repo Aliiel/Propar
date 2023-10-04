a1ed66cdfc4a5eb67d13d2e3d49eeb36a4d46a46
Aliiel <leila.mouaci@gmail.com>
2023-10-04 13:40:08 +0200

    avancée requete ajax

--- src/Form/AjoutClientFormType.php
+++ src/Form/AjoutClientFormType.php
@@ -4,8 +4,7 @@ namespace App\Form;
 
 use App\Entity\Client;
 use App\Repository\ClientRepository;
-use Doctrine\DBAL\Types\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
