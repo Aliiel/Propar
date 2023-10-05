13504ec4c3d9dff511a6d872ce3af1752389e692
Aliiel <leila.mouaci@gmail.com>
2023-10-05 13:16:53 +0200

    show operation

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -28,4 +28,6 @@ class AccueilController extends AbstractController
             'operations' => $operations,'operationsEnds' => $operationsEnds,
         ]);
     }
+
+    
 }
---
+++ src/Form/OperationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Operation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OperationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type')
+            ->add('etat')
+            ->add('date_realisation')
+            ->add('client')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Operation::class,
+        ]);
+    }
+}
